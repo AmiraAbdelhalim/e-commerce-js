cc52a598143961038da2a4766495728d139e2539
amira <amira.a.a.hashim@gmail.com>
2020-03-07 23:35:46 +0200

    page number

--- home2.js
+++ home2.js
@@ -1,12 +1,14 @@
-const div=document.querySelector("#section");
-const nextBtn=document.querySelector('#nextBtn')
-const prevBtn=document.querySelector('#preBtn')
+const divProducts=document.querySelector("#section");
+// const nextBtn=document.querySelector('#nextBtn')
+// const prevBtn=document.querySelector('#preBtn')
+const continerDiv=document.querySelector("#continer");
+const btn=document.querySelector("#btn");
 const row = document.createElement("div");
 row.className='row';
 let count=1;
 function htmlEl(products){
     // const products=response.data;//array of products
-    console.log(products.length);
+    // console.log(products.length);
     for (let i=0; i< products.length; i++){
         
         
@@ -30,9 +32,37 @@ function htmlEl(products){
         col.appendChild(cartBtn)
         row.appendChild(col);
         
-        div.appendChild(row);
-        // document.body.appendChild(div);
+        divProducts.appendChild(row);
     }
+    //buttons create buttons here and the page count
+    const nextBtn=document.createElement("button");
+    nextBtn.textContent='NEXT';
+    nextBtn.className="justify-content-end p-2 btn btn-outline-dark";
+    nextBtn.addEventListener("click", (e)=>getNextPage());
+    
+
+    const prevBtn=document.createElement("button");
+    prevBtn.textContent='PREVIOUS';
+    prevBtn.className="justify-content-start p-2 btn btn-outline-dark";
+    prevBtn.addEventListener("click", (e)=>getPrevPage());
+
+
+    const pageNum=document.createElement("p");
+    pageNum.textContent="Page: "+count;
+    pageNum.className="justify-content-center";
+
+    if (count ===1){
+        btn.append( pageNum,nextBtn);
+        // console.log(count, nextBtn);
+        
+    }else if(count ===13){
+        btn.append(prevBtn, pageNum);
+    }else{
+        btn.append(prevBtn,pageNum,nextBtn);
+        console.log(count, nextBtn);
+
+    }
+
 }
 
 
@@ -41,49 +71,51 @@ $.ajax({
     url:"https://afternoon-falls-30227.herokuapp.com/api/v1/products"
 })
 .done(function(response){
-    console.log(response);
+    // console.log(response);
     
     const products=response.data;
-    console.log(products);
+    // console.log(products);
     
     htmlEl(products);
 })           
 
 function getNextPage(){
-    div.innerHTML="";
+    divProducts.innerHTML="";
     row.innerHTML="";
+    btn.innerHTML="";
+
     count++;
     $.ajax({
         method:"GET",
         url:`https://afternoon-falls-30227.herokuapp.com/api/v1/products?page=${count}`
     })
     .done(function(response){
-        console.log(response);
+        // console.log(response);
         
         const products=response.data;
-        console.log(products);
+        // console.log(products);
         
         htmlEl(products);
     }) 
 }
 
 function getPrevPage(){
-    div.innerHTML="";
+    divProducts.innerHTML="";
     row.innerHTML="";
+    btn.innerHTML="";
     count--;
     $.ajax({
         method:"GET",
         url:`https://afternoon-falls-30227.herokuapp.com/api/v1/products?page=${count}`
     })
     .done(function(response){
-        console.log(response);
+        // console.log(response);
         
         const products=response.data;
-        console.log(products);
+        // console.log(products);
         
         htmlEl(products);
     }) 
 }
 
-nextBtn.addEventListener("click", (e)=>getNextPage());
-prevBtn.addEventListener("click", (e)=>getPrevPage());
+// prevBtn.addEventListener("click", (e)=>getPrevPage());
