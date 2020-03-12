c65e51beba6d728c5f4cac33b518db699a23d695
Nouran <nourans.96@yahoo.com>
2020-03-12 13:06:14 +0200

    Basic styling for homepage

--- js/home2.js
+++ js/home2.js
@@ -1,7 +1,7 @@
 const divProducts=document.querySelector("#section");
 const continerDiv=document.querySelector("#continer");
 const btn=document.querySelector("#btn");
-const row = document.createElement("div");//to add each product to a div
+let row = document.createElement("div");//to add each product to a div
 row.className='row';//to give it a class
 let count=1;//page count
 let cartCount=0;//cart count to add it to the key in session storage
@@ -26,20 +26,30 @@ searchBtn.addEventListener('click', (ev) => {
 //function to render html
 function htmlEl(products, search=false){
 
-    row.innerHTML = "";
+    divProducts.innerHTML = "";
     btn.innerHTML = "";
 
     // console.log(products.length)
     
     for (let i=0; i< products.length; i++){//render html to each product
+
+        if(i == 0 || i % 3 == 0) {
+            row = document.createElement('div');
+            row.classList.add('row', 'justify-content-center');
+        }
+
         const col=document.createElement("div");
-        col.className='col-md-6';
+        col.className='col-lg-3 col-md-5 card p-4 m-1';
         const img=document.createElement("img");
         // const curruncyCode=document.createElement("span");
         const contView=document.createElement("a");
-        const cartDiv=document.createElement("div");//div for cart button
+        const imageCont = document.createElement('figure');
+        const priceCont = document.createElement('div');
+        priceCont.className = 'row justify-content-between';
+        // contView.className = 'col-md-12';
+        // const cartDiv=document.createElement("div");//div for cart button
         const price=document.createElement("p");
-        const name=document.createElement("p");
+        const name=document.createElement("h5");
         const cartBtn=document.createElement("button");
         name.textContent=products[i].Name;
         img.setAttribute("src",products[i].ProductPicUrl);
@@ -49,14 +59,18 @@ function htmlEl(products, search=false){
         contView.setAttribute('href', `details.html?ProductId=${products[i].ProductId}`);//to add view page
         // price.appendChild(curruncyCode);
 
-        col.appendChild(name);
-        col.appendChild(img);
-        col.appendChild(price);
-        contView.appendChild(col);
-        cartDiv.appendChild(cartBtn)
+        // col.appendChild(name);
+        // col.appendChild(img);
+        // col.appendChild(price);
         // contView.appendChild(col);
-        row.appendChild(contView);
-        row.appendChild(cartDiv);
+        imageCont.appendChild(img);
+        priceCont.append(price, cartBtn);
+        contView.append(name, imageCont);
+        col.append(contView, priceCont);
+        // cartDiv.appendChild(cartBtn)
+        // contView.appendChild(col);
+        row.appendChild(col);
+        // row.appendChild(cartDiv);
         // row.appendChild(cartBtn)
         
         divProducts.appendChild(row);
@@ -64,6 +78,7 @@ function htmlEl(products, search=false){
         
         cartBtn.addEventListener("click", (e)=>{//adding product to session storage
             // console.log(products);
+            // e.stopPropagation();
             cartCount++;
             sessionStorage.setItem(cartCount,JSON.stringify(products[i]));
         })
@@ -73,21 +88,21 @@ function htmlEl(products, search=false){
         //adding next , previous button and page number
         const nextBtn=document.createElement("button");
         nextBtn.textContent='NEXT';
-        nextBtn.className="btn btn-outline-dark col-md-2";
+        nextBtn.className="btn btn-outline-dark mx-3";
         //justify-content-end p-2 
         nextBtn.addEventListener("click", (e)=>getNextPage());
         
 
         const prevBtn=document.createElement("button");
         prevBtn.textContent='PREVIOUS';
-        prevBtn.className=" btn btn-outline-dark col-md-2";
+        prevBtn.className=" btn btn-outline-dark mx-3";
         //justify-content-start p-2
         prevBtn.addEventListener("click", (e)=>getPrevPage());
 
 
-        const pageNum=document.createElement("p");
-        pageNum.textContent="Page: "+count;
-        pageNum.className="col-md-2";
+        const pageNum=document.createElement("span");
+        pageNum.textContent="Page "+count;
+        // pageNum.className="align-middle";
 
         //check whether to show next , previous or both
         if (count ===1){
