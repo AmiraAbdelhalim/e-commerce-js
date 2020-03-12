22de7ec18351c6c1bf70e80617c1d9bf3bce5fc6
nouran <nouranebrahim9@gmail.com>
2020-03-12 20:45:12 +0200

    some changes

--- test.js
+++ test.js
@@ -17,53 +17,82 @@ xhr.onload=()=>{
     let divv2 =document.createElement("div");
     let div =document.createElement("div");
     let divv3 =document.createElement("div");
+    let imageDiv = document.createElement('figure');
+    imageDiv.classList.add('m-auto');
     let btn=document.createElement("button");
     let i=document.createElement("i");
+    let h2_name=document.createElement("h2");
+    let h5_price=document.createElement("h5");
+    let p_quantity=document.createElement("p");
+    let p_discription=document.createElement("p");
+    let p_category=document.createElement("p");
+
   
     for (const property in res) {
-        let p =document.createElement("p");
-        if(property=="Price"){
-            divv3.appendChild(p);
-            p.setAttribute("class","display-2 text-secondary text-center");
-            p.textContent=res[property]+"$"+"\n";
+        //let p =document.createElement("p");
+        if(property=="Name"){
+            
+            h2_name.textContent=res[property];
+
+        }else if(property=="Price"){
+            //divv3.appendChild(p);
+
+            h5_price.setAttribute("class","text-secondary");
+            h5_price.textContent=res[property]+"$"+"\n";
         }else if(property=="Quantity"){
-            divv3.appendChild(p);
-            p.setAttribute("class","text-secondary my-auto");
-            p.textContent=property + "\n"+res[property];
+            //divv3.appendChild(p);
+            p_quantity.setAttribute("class","text-secondary my-auto");
+            p_quantity.textContent=property + "\n"+res[property];
 
 
-        }else if(property=="Category"||property=="Description"||property=="Name"){
-            divv.appendChild(p);
-            p.textContent=property + " : "+res[property];
-        
+        }else if(property=="Description"){
+           // divv3.appendChild(p);
+           p_discription.textContent=res[property];     
+        }else if(property=="Category"){
+            p_category.textContent=res[property]; 
         }
     }
     let image=document.createElement("img");
     
     
     b.appendChild(div);
-    b.appendChild(div); 
+    // b.appendChild(div); 
     div.appendChild(div2);
    
     div2.appendChild(divv2);
     div2.appendChild(divv);
     div2.appendChild(divv3);
-    divv2.appendChild(image);
-    divv.appendChild(btn);
+    imageDiv.appendChild(image);
+    divv2.appendChild(imageDiv);
+    divv3.appendChild(h2_name);
+    divv3.appendChild(h5_price);
+    divv3.appendChild(p_discription);
+    divv3.appendChild(p_category);
+    divv3.appendChild(p_quantity);
+
+    divv3.appendChild(btn);
+    // div.appendChild(btn);
     btn.appendChild(i);
 
-    b.setAttribute("style","margin-top:250px");
+
+    h2_name.setAttribute("class","mt-3");
+    h5_price.setAttribute("class","mt-3");
+    p_category.setAttribute("class","mt-3");
+    p_discription.setAttribute("class","mt-3");
+    p_quantity.setAttribute("class","mt-3");
+
+
     div.setAttribute("class","container");
-    div2.setAttribute("class","row");
+    div2.setAttribute("class","row align-items-stretch");
   
    
-    divv2.setAttribute("class","col");
-    divv.setAttribute("class","col my-auto");
-    divv3.setAttribute("class","col border border-info rounded w-25 card ");
+    divv2.setAttribute("class","col-md-6 card image-card");
+    // divv.setAttribute("class","col-md-6 my-auto");
+    divv3.setAttribute("class","col-md-6 pl-5");
     
     image.setAttribute("src",res.ProductPicUrl);
-    //btn.innerHTML="<i class='fad fa-shopping-cart'></i>";
-    btn.textContent="card";
+    btn.innerHTML="<i class='fa fa-shopping-cart'></i>";
+    // btn.textContent="card";
     btn.addEventListener("click",(ev)=>{
         sessionStorage.setItem(sessionStorage.length,JSON.stringify(res));
     })
