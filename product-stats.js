77cb24581e520b0a9c513c19ab1a3a1b888ee982
Ahmed Mamdouh <ahmedmamdouh2727@gmail.com>
2020-03-13 20:22:14 +0200

    added filter by category and filter by supplierName features

--- product-stats.js
+++
@@ -1,9 +0,0 @@
-let req = new XMLHttpRequest();
-
-req.open('GET', ' https://afternoon-falls-30227.herokuapp.com/api/v1/products-stats/');
-req.send();
-req.onlaod = function() {
-    var res =JSON.parse(this.response);
-    console.log(res);  
-};
-
